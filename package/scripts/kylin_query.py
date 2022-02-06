69a7336226fddf2a4fbdb41aebafdc5f785edf1e
juanp <34025582+barbyware@users.noreply.github.com>
2022-02-06 09:36:49 +0100

    Update kylin_query.py

--- package/scripts/kylin_query.py
+++ package/scripts/kylin_query.py
@@ -38,7 +38,7 @@ class KylinQuery(Script):
         import params
         env.set_params(params)
         self.configure(env)
-        Execute(format(". {tmp_dir}/kylin_env.rc;{install_dir}/latest/bin/kylin.sh start"))
+        Execute(format("su -u kylin '. {tmp_dir}/kylin_env.rc;{install_dir}/latest/bin/kylin.sh start'"))
         sleep(5)
         Execute("ps -ef | grep java | grep kylin | grep -v grep | awk '{print $2}'>"+format("{install_dir}/latest/pid"))
         Execute(format("rm -rf /var/run/kylin.pid;cp {install_dir}/latest/pid /var/run/kylin.pid"))
@@ -48,7 +48,7 @@ class KylinQuery(Script):
         import params
         env.set_params(params)
         self.configure(env)
-        Execute(format(". {tmp_dir}/kylin_env.rc;{install_dir}/latest/bin/kylin.sh stop"))
+        Execute(format("su -u kylin '. {tmp_dir}/kylin_env.rc;{install_dir}/latest/bin/kylin.sh stop'"))
 
 
     def restart(self, env):
