d0af26cd894b17442f755310a2ae6c0f1bbce49f
juanp <34025582+barbyware@users.noreply.github.com>
2022-03-28 12:50:49 +0200

    Update kylin_master.py

--- package/scripts/kylin_master.py
+++ package/scripts/kylin_master.py
@@ -33,6 +33,8 @@ class KylinMaster(Script):
         env.set_params(params)
         kylin_properties = InlineTemplate(params.kylin_properties)   
         File(format("{install_dir}/latest/conf/kylin.properties"), content=kylin_properties)
+	kylin_hive_conf=service_packagedir+'/configuration/kylin_hive_conf.xml'
+	File(format("{install_dir}/latest/conf/kylin_hive_conf.xml"), content= kylin_hive_conf)
         
         File(format("{tmp_dir}/kylin_init.sh"),
              content=Template("init.sh.j2"),
